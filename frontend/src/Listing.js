9a66ab89b176ebc35571844efd8ba547c829955c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-05 15:03:25 +0100

    added files

--- frontend/src/Listing.js
+++ frontend/src/Listing.js
@@ -2,32 +2,108 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-function Listing() {
-  const [listings, setListings] = useState([]);
+const Listing = () => {
+  const [propertyListings, setPropertyListings] = useState([]);
+  const [propertyTypeFilter, setPropertyTypeFilter] = useState('');
+  const [priceRangeFilter, setPriceRangeFilter] = useState('');
+  const [bedroomsFilter, setBedroomsFilter] = useState('');
+  const [bathroomsFilter, setBathroomsFilter] = useState('');
+
+  const fetchPropertyListings = async () => {
+    try {
+      // Build the query string based on the selected filters
+      let queryString = '/api/property-listings?';
+      if (propertyTypeFilter) {
+        queryString += `propertyType=${propertyTypeFilter}&`;
+      }
+      if (priceRangeFilter) {
+        queryString += `priceRange=${priceRangeFilter}&`;
+      }
+      if (bedroomsFilter) {
+        queryString += `bedrooms=${bedroomsFilter}&`;
+      }
+      if (bathroomsFilter) {
+        queryString += `bathrooms=${bathroomsFilter}&`;
+      }
+
+      // Send the API request with the query string
+      const response = await fetch(queryString);
+      if (response.ok) {
+        const data = await response.json();
+        setPropertyListings(data);
+      } else {
+        console.error('Failed to fetch property listings');
+      }
+    } catch (error) {
+      console.error('Failed to fetch property listings:', error);
+    }
+  };
 
   useEffect(() => {
-    // Fetch data from API or use mock data
-    // Example API call using fetch:
-    fetch('https://api.example.com/listings')
-      .then(response => response.json())
-      .then(data => setListings(data))
-      .catch(error => console.log(error));
-  }, []);
+    fetchPropertyListings();
+  }, [propertyTypeFilter, priceRangeFilter, bedroomsFilter, bathroomsFilter]);
+
+  // ...
 
   return (
     <div>
-      <h1>Real Estate Listings</h1>
-      {listings.map(listing => (
-        <div key={listing.id}>
-          <h2>{listing.title}</h2>
-          <p>{listing.address}</p>
-          <p>{listing.description}</p>
-        </div>
-      ))}
-      <Header />
-      <Footer />
+      <h1>Property Listings</h1>
+      <div>
+        {/* Property Type Filter */}
+        <label>Property Type:</label>
+        <select
+          value={propertyTypeFilter}
+          onChange={(e) => setPropertyTypeFilter(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="house">House</option>
+          <option value="apartment">Apartment</option>
+          {/* Add other property types */}
+        </select>
+
+        {/* Price Range Filter */}
+        <label>Price Range:</label>
+        <input
+          type="text"
+          value={priceRangeFilter}
+          onChange={(e) => setPriceRangeFilter(e.target.value)}
+        />
+
+        {/* Bedrooms Filter */}
+        <label>Bedrooms:</label>
+        <input
+          type="text"
+          value={bedroomsFilter}
+          onChange={(e) => setBedroomsFilter(e.target.value)}
+        />
+
+        {/* Bathrooms Filter */}
+        <label>Bathrooms:</label>
+        <input
+          type="text"
+          value={bathroomsFilter}
+          onChange={(e) => setBathroomsFilter(e.target.value)}
+        />
+
+        <button onClick={fetchPropertyListings}>Apply Filters</button>
+      </div>
+
+      {/* Display property listings */}
+      {propertyListings.length > 0 ? (
+        <ul>
+          {propertyListings.map((property) => (
+            <li key={property.id}>
+              <p>{property.title}</p>
+              <p>{property.description}</p>
+              {/* Display other property details */}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No property listings found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default Listing;
