7219d5aa65c54b6e8cab907e669389e949d99efe
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-06 13:04:47 +0100

    added files

---
+++ frontend/src/api.js
@@ -0,0 +1,66 @@
+const API_BASE_URL = 'http://localhost:8000'; // Replace with your actual backend server URL
+
+const api = {
+  fetchProperties: async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/properties`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Failed to fetch properties:', error);
+      throw error;
+    }
+  },
+
+  createProperty: async (propertyData) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/properties`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(propertyData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Failed to create property:', error);
+      throw error;
+    }
+  },
+
+  updateProperty: async (propertyId, updatedData) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/properties/${propertyId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Failed to update property with ID ${propertyId}:`, error);
+      throw error;
+    }
+  },
+
+  deleteProperty: async (propertyId) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/properties/${propertyId}`, {
+        method: 'DELETE',
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Failed to delete property with ID ${propertyId}:`, error);
+      throw error;
+    }
+  },
+
+  // Add more custom API functions for your specific endpoints and requirements
+};
+
+export default api;
+
